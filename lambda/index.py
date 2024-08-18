034adcabc12840c3f16db80ae62340d5eb3e4024
nitink23 <50839741+nitink23@users.noreply.github.com>
2024-08-18 13:18:54 -0700

    sales volume

--- lambda/index.py
+++ lambda/index.py
@@ -25,51 +25,7 @@ SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_ANON_KEY = os.getenv('SUPABASE_ANON_KEY')
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
 
-args = [
-    '--disable-web-security',
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-    '--disable-dev-shm-usage',
-    '--disable-gpu',
-    '--no-zygote',
-    '--single-process',
-    '--disable-software-rasterizer',
-    '--disable-extensions',
-    '--disable-background-networking',
-    '--disable-background-timer-throttling',
-    '--disable-backgrounding-occluded-windows',
-    '--disable-breakpad',
-    '--disable-client-side-phishing-detection',
-    '--disable-component-extensions-with-background-pages',
-    '--disable-default-apps',
-    '--disable-features=TranslateUI,BlinkGenPropertyTrees',
-    '--disable-hang-monitor',
-    '--disable-ipc-flooding-protection',
-    '--disable-popup-blocking',
-    '--disable-prompt-on-repost',
-    '--disable-renderer-backgrounding',
-    '--disable-sync',
-    '--force-color-profile=srgb',
-    '--metrics-recording-only',
-    '--no-first-run',
-    '--safebrowsing-disable-auto-update',
-    '--enable-automation',
-    '--password-store=basic',
-    '--use-mock-keychain',
-    '--disable-component-update',
-    '--disable-domain-reliability',
-    '--disable-print-preview',
-    '--disable-site-isolation-trials',
-    '--disable-speech-api',
-    '--disk-cache-size=33554432',
-    '--enable-features=SharedArrayBuffer',
-    '--hide-scrollbars',
-    '--ignore-gpu-blocklist',
-    '--mute-audio',
-    '--no-default-browser-check',
-    '--no-pings',
-    '--window-size=1920,1080',
-]
+args=['--no-sandbox', '--disable-setuid-sandbox','--single-process','--disable-gpu']
 
 
 def install_dependencies():
@@ -359,8 +315,72 @@ def scrape_address_data(address,report_id):
 
     return metrics
 
+def get_rent_insights(address, sqft, report_id ,listing_type="for_rent", past_days=300, type = 1):
+    """
+    Get insights on the best rent for a particular property based on the rent to square footage ratio.
+    
+    Args:
+        address (str): The address of the property (city, state, zip).
+        listing_type (str): The type of listings to search (for_sale, for_rent, pending).
+        past_days (int): How many past days to include in the search.
+    
+    Returns:
+        dict: Dictionary with property details and rent per square foot.
+    """
+    # Fetch properties based on the address
+    properties = scrape_property(
+        location=address,
+        radius=10,
+        listing_type=listing_type,
+        past_days=past_days,
+    )
+    
+    if properties.empty:
+        return {"message": "No properties found for the given address."}
+
+    # Filter out properties with missing 'list_price' or 'sqft'
+    filtered_properties = properties[(properties['list_price'].notna()) & (properties['sqft'].notna()) & (properties['lot_sqft'].notna())]
+    print(len(filtered_properties))
+    # Initialize a dictionary to store insights
+    properties_rent = filtered_properties['list_price'].mean()
+    properties_sqft_rent = filtered_properties['list_price']/filtered_properties['sqft']
+    properties_sqft_rent_lot = filtered_properties['lot_sqft']/filtered_properties['sqft']
+    if type == 1: 
+        estimated_rent = sqft*properties_sqft_rent.mean()
+    else: 
+        estimated_rent = sqft*properties_sqft_rent_lot.mean()
+    
+    fifty_pct_rule = estimated_rent*0.5
+    rent_cash_flow = {
+        'estimated_rent':estimated_rent,
+        'fifty_pct_rule': fifty_pct_rule,
+        'rent_per_sqft':properties_sqft_rent.mean(),
+        'rent_per_lot_sqft': properties_sqft_rent_lot.mean(),
+        'basis_number':len(filtered_properties)
+
+        # mortgage_costs should be in here and appreciation
+    }
 
 
+    supabase.table('reports').update({
+        'rent_cash_flow': json.dumps(rent_cash_flow)
+    }).eq('id', report_id).execute()
+
+    print("Rent cash flow successfully upload")
+    return rent_cash_flow
+
+def get_average_days_on_market(address):
+    listing_type = "sold"
+    sales_volume_six_months = []
+    for i in range(1,6):
+        properties = scrape_property(
+            location=address,
+            radius=10,
+            listing_type=listing_type,
+            past_days=30*i,
+        )
+        sales_volume_six_months.append(len(properties))
+    return sales_volume_six_months
 
 
 def fetch_city_census_data(city_name, report_id):
@@ -475,6 +495,8 @@ def handler(event, context):
         zipcode = listing['zip_code']
         lat = listing['latitude']
         long = listing['longitude']
+        sqft = listing['sqft']
+        lot_sqft = listing['lot_sqft']
         address = f'{street_line},{city},{state} {zipcode}'
 
         # install_dependencies()
@@ -509,11 +531,20 @@ def handler(event, context):
         else:
             print(f"Failed to upload census data for {city}")
 
+        if sqft == -1:
+           rent_cash_flow = get_rent_insights(address, lot_sqft, report_id ,listing_type="for_rent", past_days=300, type = 2)
+        else:
+            rent_cash_flow = get_rent_insights(address, sqft, report_id ,listing_type="for_rent", past_days=300, type = 1)
+        if rent_cash_flow:
+            print(f"Successfully uploaded rent cash flow data for {city}. Here is the data:{rent_cash_flow}")
+        else:
+            print(f"Failed to upload rent cash flow data for {city}")
+
+
         
         
-        # Example of using Supabase client
-        data = supabase.table('your_table').select('*').execute()
-        print(f"Supabase data: {data}")
+        
+       
 
     return {
         'statusCode': 200,
